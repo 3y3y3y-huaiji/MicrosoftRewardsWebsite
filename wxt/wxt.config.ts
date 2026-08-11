c83cabc13074b77dee8d3c77652ad4ec3c885327
Developer <developer@example.com>
2026-08-11 17:36:35 +0800

    chore(dev): update manifest name to Dev版 and version to 3.0.4

--- wxt/wxt.config.ts
+++ wxt/wxt.config.ts
@@ -5,9 +5,9 @@ export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   alias: { '@': '.' },
   manifest: {
-    name: '微软 Rewards 自动搜索器',
+    name: '微软 Rewards 自动搜索器 (Dev版)',
     description:
-      '自动或一键获取每日最高微软 Rewards 积分的浏览器扩展。',
+      '【Dev版-WXT 0.21.3】自动或一键获取每日最高微软 Rewards 积分的浏览器扩展。',
     permissions: ['storage', 'alarms'],
     icons: { 16: 'imgs/logo.png', 32: 'imgs/logo2.png', 48: 'imgs/logo2.png', 128: 'imgs/logo3.png' },
     action: {
